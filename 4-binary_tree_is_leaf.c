7489dc559c10eea9c2f23501fcd591838ab92a0c
yassineboucham <yassineboucham2002@gmail.com>
2024-08-14 17:27:21 +0100

    leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,17 +2,18 @@
 /**
 	* binary_tree_is_leaf - deletes an entire binary tree
 	* @node: const binary_tree_t
-    * Return: 1 if node is a leaf, otherwise 0
+	* Return: 1 if node is a leaf, otherwise 0
 */
 
-int binary_tree_is_leaf(const binary_tree_t *node){
-    if (node == NULL)
-    {
-        return 0;
-    }
-    if (node->left == NULL && node->right == NULL)
-    {
-        return 1;
-    }
-    return 0;
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
