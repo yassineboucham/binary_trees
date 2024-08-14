13cfe08ad0a28cd08464449321ab7c1208e6d847
yassineboucham <yassineboucham2002@gmail.com>
2024-08-14 21:23:05 +0100

    goes through a binary tree using pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-    func(tree->n);
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
