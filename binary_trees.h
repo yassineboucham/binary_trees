aa950f349472b2221954c0d9d2edc7f38a174387
yassineboucham <yassineboucham2002@gmail.com>
2024-08-14 15:24:57 +0100

    1

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -22,5 +23,6 @@ typedef struct binary_tree_s
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
