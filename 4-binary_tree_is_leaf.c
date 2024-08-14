689bd0564d97f3d1272cda33988dbd734f523b81
yassineboucham <yassineboucham2002@gmail.com>
2024-08-14 17:25:00 +0100

    leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+	* binary_tree_is_leaf - deletes an entire binary tree
+	* @node: const binary_tree_t
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node){
+    if (node == NULL)
+    {
+        return 0;
+    }
+    if (node->left == NULL && node->right == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
