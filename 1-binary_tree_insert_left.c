97d8ac5364e4f7601140bffcc306527b1f5e735a
yassineboucham <yassineboucham2002@gmail.com>
2024-08-14 15:33:04 +0100

    malloc solved

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,12 +15,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
